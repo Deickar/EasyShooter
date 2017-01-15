e2dba93bf7f4249619f6d8b46509387825cbc177
AfroStalin <chernecoff@gmail.com>
2017-01-15 15:33:23 +0300

    Added bin and game folder

---
+++ Game/Libs/UI/GenerateGFX.bat
@@ -0,0 +1,7 @@
+E:\_Development\CustomCryEngine\Crytek\CRYENGINE_5.2.3\Tools\GFxExport\gfxexport.exe AuthenticationPage.swf
+E:\_Development\CustomCryEngine\Crytek\CRYENGINE_5.2.3\Tools\GFxExport\gfxexport.exe CreateProfilePage.swf
+E:\_Development\CustomCryEngine\Crytek\CRYENGINE_5.2.3\Tools\GFxExport\gfxexport.exe LoadingPage.swf
+E:\_Development\CustomCryEngine\Crytek\CRYENGINE_5.2.3\Tools\GFxExport\gfxexport.exe ErrorPage.swf
+E:\_Development\CustomCryEngine\Crytek\CRYENGINE_5.2.3\Tools\GFxExport\gfxexport.exe MainMenuPage.swf
+
+E:\_Development\CustomCryEngine\Crytek\CRYENGINE_5.2.3\Tools\GFxExport\gfxexport.exe TestUI.swf
