d1ee0df717b703a60ab9ae86aa4c66b6c8207e10
AfroStalin <chernecoff@gmail.com>
2017-02-08 12:54:07 +0300

    [AfroStalin] Fixed editor crash when jump to game

--- Code/GamePlugin.cpp
+++ Code/GamePlugin.cpp
@@ -53,10 +53,10 @@ void CGamePlugin::OnSystemEvent(ESystemEvent event, UINT_PTR wparam, UINT_PTR lp
 		}
 
 		// Start connect to FireNet
-		if (gEnv->pFireNet && !gEnv->IsEditing())
+		if (gEnv->pFireNet && !gEnv->IsEditor())
 		{
-			gEnv->pFireNet->RegisterFireNetListener(this);
-			gEnv->pFireNet->ConnectToMasterServer();
+//			gEnv->pFireNet->RegisterFireNetListener(this);
+//			gEnv->pFireNet->ConnectToMasterServer();
 		}
 
 		break;
